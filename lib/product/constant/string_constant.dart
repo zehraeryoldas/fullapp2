dd252b24b995cbee2653337ab44a5852413785c7
zehraeryoldas <eryoldas577@gmail.com>
2023-08-16 01:25:00 +0300

    string constants page added

--- lib/product/constant/string_constant.dart
+++ lib/product/constant/string_constant.dart
@@ -5,4 +5,6 @@ class StringContants {
   //bütün metotlar private olacağı için bunu kapatıyoruz
   const StringContants._();
   static const String appName = "Nuntium";
+  static const loginWelcomeBack = "Welcome Back ";
+  static const loginwelcomDetail = "Seni yeniden gördüğüme mennun oldum";
 }
