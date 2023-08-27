99f0e52795535217ee78334a7db4753ccad8d0dc
zehraeryoldas <eryoldas577@gmail.com>
2023-08-27 22:52:51 +0300

    firebaseutility page created

---
+++ lib/product/utility/firebase/firebase_utilty.dart
@@ -0,0 +1,23 @@
+import 'package:fullapp2/product/utility/base/base_model.dart';
+import 'package:fullapp2/product/utility/firebase/firebase_collections.dart';
+
+mixin FirebaseUtility {
+  Future<List<T>?> fetchList<T extends IdModel, R extends BaseFirebaseModel<T>>(
+    R data,
+    FirebaseCollections collections,
+  ) async {
+    final newsCollectionRef = FirebaseCollections.news.ref;
+    final response = await newsCollectionRef.withConverter<T>(
+        fromFirestore: (snapshot, options) {
+      return data.fromFirebase(snapshot);
+    }, toFirestore: (value, options) {
+      return {};
+    }).get();
+
+    if (response.docs.isNotEmpty) {
+      final values = response.docs.map((e) => e.data()).toList();
+      return values;
+    }
+    return null;
+  }
+}
